f2c8ea17e456e8fcec3804b7466050067e30fe19
vo-kim-nguyen <vo.kim.nguyen@linecorp.com>
2021-09-08 14:41:58 +0700

    Prepare component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div id="app">
+        <MainGame />
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import {Component, Vue} from 'vue-property-decorator';
+import MainGame from "@/views/MainGame.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        MainGame
+    },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/views/MainGame.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>ABC</h1>
+    </div>
+</template>
+
+<script>
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({})
+export default class MainGame extends Vue {
+
+}
+</script>
